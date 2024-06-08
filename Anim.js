e09eb902799fc5bb0aef3ce3d60a113d9379503c
yerkin-amanzhol <yerkin.amanzhol@gmail.com>
2024-06-08 17:42:27 +0200

    Add files via upload

---
+++ Anim.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+  
+    sections.forEach(function(section) {
+      const sectionPosition = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+  
+      if (sectionPosition < windowHeight * 0.8) {
+        section.classList.add('animate-on-scroll');
+      }
+    });
+  });
