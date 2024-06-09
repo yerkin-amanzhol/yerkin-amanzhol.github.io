a14ef4145dbcc8319a46baf9cdda1e90fba7e2a6
yerkin-amanzhol <yerkin.amanzhol@gmail.com>
2024-06-09 20:37:53 +0200

    Update Anim.js

--- Anim.js
+++ Anim.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('section');
-  
-    sections.forEach(function(section) {
-      const sectionPosition = section.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-  
-      if (sectionPosition < windowHeight * 0.8) {
-        section.classList.add('animate-on-scroll');
-      }
-    });
+  const sections = document.querySelectorAll('section');
+
+  sections.forEach(function(section) {
+    const sectionPosition = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionPosition < windowHeight * 0.8) {
+      section.classList.add('animate-on-scroll', 'fade-in');
+    }
   });
+});
